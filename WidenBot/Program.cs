1ae5a0e1e4d30dea919c468beed4a7c56e16b9ca
Cristian Widenhouse <cgwidenhouse@gmail.com>
2023-11-30 06:03:31 +0000

    Initial (#1)
    Co-authored-by: Cristian Widenhouse <cristian.widenhouse@moovila.com>

---
+++ WidenBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WidenBot
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
