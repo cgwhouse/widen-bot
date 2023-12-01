37991a3341d91d07cdddac138b62e4a86fff32ed
Cristian Widenhouse <cristian.widenhouse@moovila.com>
2023-12-01 00:49:34 -0500

    remove unnecessary from constants

--- WidenBot/Constants.cs
+++ WidenBot/Constants.cs
@@ -5,6 +5,4 @@ internal static class Constants
     public static string DiscordBotToken = "<DISCORD_BOT_TOKEN>";
     public static string DiscordServerID = "<DISCORD_SERVER_ID>";
     public static string LavalinkPassword = "<LAVALINK_PASSWORD>";
-    public static string YouTubeEmail = "<YOUTUBE_EMAIL>";
-    public static string YouTubePassword = "<YOUTUBE_PASSWORD>";
 }
