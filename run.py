fd390e97ed84287206c4ebb8a92918d371504267
Cristian Widenhouse <cristian.widenhouse@moovila.com>
2024-06-06 17:15:52 -0400

    wip

--- run.py
+++ run.py
@@ -14,48 +14,34 @@ import sys
 
 
 def main():
-    run_target = handle_client_server_arg()
+    # if len(sys.argv) < 2:
+    #    return None
 
-    if run_target is None:
+    # Handle run target
+    try:
+        run_target = sys.argv[1].lower()
+
+        if run_target not in ["client", "server"]:
+            raise IndexError
+    except IndexError:
         print(
             "Usage: python3 run.py run_target, where run_target = 'client' or 'server'"
         )
         return
 
+    # Extract config.json
     user_config = handle_user_config(run_target)
 
     if user_config is None:
         print(
-            "config.json must exist in main directory (same as this script), and must be fully specified. See README.md"
+            "config.json must exist in main directory (same as this script), and must be fully specified. See 'WidenBot Config' section of README.md"
         )
         return
 
     if run_target == "client":
-        # Ensure server running first
-        serverCheck = subprocess.run(
-            ["docker", "container", "ls"], capture_output=True, text=True
-        )
-
-        if serverCheck.stdout.find(f"{user_config['label']}-widenbot-server") == -1:
-            print("Server must be running first")
-            return
-
         run_client(user_config)
     else:
-        kill_client_if_running(user_config)
         run_server(user_config)
-
-
-def handle_client_server_arg():
-    if len(sys.argv) < 2:
-        return None
-
-    run_target = sys.argv[1].lower()
-
-    if run_target not in ["client", "server"]:
-        return None
-
-    return run_target
 
 
 def handle_user_config(run_target):
@@ -118,23 +104,34 @@ def handle_password(run_target):
 
 
 def run_client(user_config):
-    write_file_contents("Client/config.json", user_config, is_json=True)
+    # Ensure server running first
+    serverCheck = subprocess.run(
+        ["docker", "container", "ls"], capture_output=True, text=True
+    )
+
+    if serverCheck.stdout.find(f"{user_config['label']}-widenbot-server") == -1:
+        print(
+            "Server must be running first, See 'Running the Bot' section of README.md"
+        )
+        return
+
+    # write_file_contents("Client/config.json", user_config, is_json=True)
 
     # Change current working directory to client
     os.chdir("./Client")
 
-    instance_label = "INSTANCE_LABEL"
-    client_port = "CLIENT_PORT"
+    # instance_label = "INSTANCE_LABEL"
+    # client_port = "CLIENT_PORT"
 
-    dockerComposeRaw = get_file_contents("docker-compose.template.yaml")
+    # dockerComposeRaw = get_file_contents("docker-compose.template.yaml")
 
-    dockerComposeUpdated = dockerComposeRaw.replace(
-        instance_label, user_config["label"]
-    ).replace(client_port, user_config["clientPort"])
+    # dockerComposeUpdated = dockerComposeRaw.replace(
+    #    instance_label, user_config["label"]
+    # ).replace(client_port, user_config["clientPort"])
 
-    write_file_contents("docker-compose.yaml", dockerComposeUpdated)
+    # write_file_contents("docker-compose.yaml", dockerComposeUpdated)
 
-    print("docker-compose.yaml has been created / overwritten...")
+    # print("docker-compose.yaml has been created / overwritten...")
 
     print("Building and running WidenBot client...")
 
@@ -143,39 +140,55 @@ def run_client(user_config):
 
 
 def run_server(user_config):
+    kill_client_if_running(user_config)
+
+    # Write config.json contents to .env file
+    write_env_file(user_config)
+
     # Change current working directory to server
     os.chdir("./Server")
 
     # Create application.yml and docker-compose.yaml with injected secrets from config
-    lavalink_password = "LAVALINK_PASSWORD"
+    # lavalink_password = "LAVALINK_PASSWORD"
     spotify_client_id = "SPOTIFY_CLIENTID"
     spotify_client_secret = "SPOTIFY_CLIENTSECRET"
-    instance_label = "INSTANCE_LABEL"
+    # instance_label = "INSTANCE_LABEL"
 
     lavalinkConfigRaw = get_file_contents("application.template.yml")
 
     lavalinkConfigUpdated = (
-        lavalinkConfigRaw.replace(lavalink_password, user_config["password"])
-        .replace(spotify_client_id, user_config["spotify"]["clientID"])
-        .replace(spotify_client_secret, user_config["spotify"]["clientSecret"])
+        # lavalinkConfigRaw.replace(lavalink_password, user_config["password"])
+        lavalinkConfigRaw.replace(
+            spotify_client_id, user_config["spotify"]["clientID"]
+        ).replace(spotify_client_secret, user_config["spotify"]["clientSecret"])
     )
 
     write_file_contents("application.yml", lavalinkConfigUpdated)
 
-    print("application.yml has been created / overwritten...")
+    print("application.yml has been created / overwritten with Spotify credentials...")
 
-    dockerComposeRaw = get_file_contents("docker-compose.template.yaml")
+    # dockerComposeRaw = get_file_contents("docker-compose.template.yaml")
 
-    dockerComposeUpdated = dockerComposeRaw.replace(
-        lavalink_password, user_config["password"]
-    ).replace(instance_label, user_config["label"])
+    # dockerComposeUpdated = dockerComposeRaw.replace(
+    #    lavalink_password, user_config["password"]
+    # ).replace(instance_label, user_config["label"])
 
-    write_file_contents("docker-compose.yaml", dockerComposeUpdated)
+    # write_file_contents("docker-compose.yaml", dockerComposeUpdated)
 
-    print("docker-compose.yaml has been created / overwritten...")
+    # print("docker-compose.yaml has been created / overwritten...")
 
     # Run container
-    subprocess.run(["docker", "compose", "up", "--build", "--force-recreate"])
+    subprocess.run(
+        [
+            "docker",
+            "compose",
+            "up",
+            "--build",
+            "--force-recreate",
+            "-p",
+            user_config["label"],
+        ]
+    )
 
 
 def kill_client_if_running(user_config):
@@ -195,6 +208,26 @@ def kill_client_if_running(user_config):
     )
 
     print("Old client has been killed...")
+
+
+def write_env_file(user_config):
+
+    env_file_contents = ""
+
+    env_file_contents += f"CLIENT_PORT={user_config['clientPort']}\n"
+    env_file_contents += f"INSTANCE_LABEL={user_config['label']}\n"
+
+    env_file_contents += f"DISCORD_SERVER_ID={user_config['discord']['serverID']}\n"
+    env_file_contents += f"DISCORD_BOT_TOKEN={user_config['discord']['botToken']}\n"
+
+    env_file_contents += f"SPOTIFY_CLIENT_ID={user_config['spotify']['clientID']}\n"
+    env_file_contents += (
+        f"SPOTIFY_CLIENT_SECRET={user_config['spotify']['clientSecret']}\n"
+    )
+
+    write_file_contents(".env", env_file_contents)
+
+    print(".env file has been created / overwritten...")
 
 
 def get_file_contents(path):
